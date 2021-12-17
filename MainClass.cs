ade1304ccbed9166cae8026e3b65102af2257406
BABLU KUMAR <43786994+bsy2121221@users.noreply.github.com>
2021-12-17 17:57:17 +0530

    Add files via upload

---
+++ MainClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Sender
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection()) 
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                       durable: false,
+                                       autoDelete: false,
+                                       arguments: null);
+                string message = "Hello world";
+
+                //it is use to encrept the message
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "",
+                                      routingKey: "Hello",
+                                      basicProperties: null,
+                                      body: body);
+                Console.WriteLine("[x] message send {0}", message);
+            }
+            Console.WriteLine("Press enter to exit from the application");
+            Console.ReadLine();
+        }
+    }
+}
